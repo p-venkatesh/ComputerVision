c63bc033c735691799f02f41aea8b075a8208f19
P.Venkatesh <44500431+p-venkatesh@users.noreply.github.com>
2019-12-26 10:04:52 +0530

    Add files via upload

--- homework_solution.py
+++ homework_solution.py
@@ -1,36 +1,45 @@
 # Homework Solution
 
 # Importing the libraries
+import torch
+from torch.autograd import Variable
 import cv2
-
-# Loading the cascades
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+from data import BaseTransform, VOC_CLASSES as labelmap
+from ssd import build_ssd
+import imageio
 
 # Defining a function that will do the detections
-def detect(gray, frame):
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
-        for (sx, sy, sw, sh) in smiles:
-            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
+def detect(frame, net, transform):
+    height, width = frame.shape[:2]
+    frame_t = transform(frame)[0]
+    x = torch.from_numpy(frame_t).permute(2, 0, 1)
+    x = Variable(x.unsqueeze(0))
+    y = net(x)
+    detections = y.data
+    scale = torch.Tensor([width, height, width, height])
+    # detections = [batch, number of classes, number of occurence, (score, x0, Y0, x1, y1)]
+    for i in range(detections.size(1)):
+        j = 0
+        while detections[0, i, j, 0] >= 0.6:
+            pt = (detections[0, i, j, 1:] * scale).numpy()
+            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]), int(pt[3])), (255, 0, 0), 2)
+            cv2.putText(frame, labelmap[i - 1], (int(pt[0]), int(pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            j += 1
     return frame
 
-# Doing some Face Recognition with the webcam
-video_capture = cv2.VideoCapture(0)
-while True:
-    _, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    canvas = detect(gray, frame)
-    cv2.imshow('Video', canvas)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-video_capture.release()
-cv2.destroyAllWindows()
+# Creating the SSD neural network
+net = build_ssd('test')
+net.load_state_dict(torch.load('ssd300_mAP_77.43_v2.pth', map_location = lambda storage, loc: storage))
+
+# Creating the transformation
+transform = BaseTransform(net.size, (104/256.0, 117/256.0, 123/256.0))
+
+# Doing some Object Detection on a video
+reader = imageio.get_reader('epic_horses.mp4')
+fps = reader.get_meta_data()['fps']
+writer = imageio.get_writer('output.mp4', fps = fps)
+for i, frame in enumerate(reader):
+    frame = detect(frame, net.eval(), transform)
+    writer.append_data(frame)
+    print(i)
+writer.close()
